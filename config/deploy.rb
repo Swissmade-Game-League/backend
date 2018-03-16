73f151ffacc48dba520d4193562490daf0aef2c3
Clément Hampaï <clement.hampai@cypressxt.net>
2018-03-16 09:08:21 +0100

    Manage puma using Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,3 +38,15 @@ append :linked_files,'config/database.yml', 'config/secrets.yml'
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+namespace :deploy do
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+      execute :bundle, 'exec cap puma:restart'
+    end
+  end
+end
