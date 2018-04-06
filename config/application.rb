b27f200154c97b69e22ba24b9d278800715ca686
Clément Hampaï <clement.hampai@cypressxt.net>
2018-04-06 23:29:46 +0200

    Cors

--- config/application.rb
+++ config/application.rb
@@ -34,9 +34,9 @@ module SwissmadeGameLeagueBackend
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => :any
       end
     end
-    
+
   end
 end
