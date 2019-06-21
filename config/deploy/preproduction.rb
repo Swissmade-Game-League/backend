cb6596f7e0826365f23c94ddcbd09939fd8a1e98
Clément Hampaï <clement.hampai@cypressxt.net>
2019-06-22 01:27:32 +0200

    Moved to new RoR srv

--- config/deploy/preproduction.rb
+++ config/deploy/preproduction.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server 'ruby-host.cypressxt.net', user: 'cypress', roles: %w{app db web}, port: 22106
+server 'ruby-host.cypressxt.net', user: 'cypress', roles: %w{app db web}, port: 22107
 
 
 
